664badfbf57c475dbdaa52758ea9607bb3e0d723
revealations <revealations@naver.com>
2021-11-27 21:46:43 +0900

    glotech-header

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav class="w-full h-30 bg-white border-b">
+      <div class="flex mx-auto px-40">
+        <p class="text-xs">
+          로그인 | 회원가입 | 이용가이드 | 사이트맵 | 고객센터
+        </p>
+      </div>
+      <div class="flex justify-center max-w-screen-xl mx-auto">
+        <nuxt-link to="/">
+          <img class="w-55 h-20 -mt-1" src="@/static/glotecLogo.png">
+        </nuxt-link>
+      </div>
+    </nav>
+    <Nuxt />
+    <footer>
+      사업자등록번호: 1234-5-6-7-6-34534
+      카피라이트 2021
+      <!-- //Tailwind css intellisense
+        // Volar -->
+    </footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ addNumber(1, 1) }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  methods: {
+    addNumber (a: number, b: number): string {
+      return (a + b).toString()
+    }
+  }
+})
+</script>
